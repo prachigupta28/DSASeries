bcfc4e74d157f5f0740123bb2b65a485e88e3f20
prachigupta28 <74293469+prachigupta28@users.noreply.github.com>
2020-12-28 00:41:03 +0530

    refactor: added java docs and formatted the code

--- src/array/MiddleElement.java
+++ src/array/MiddleElement.java
@@ -2,15 +2,23 @@ package array;
 
 import java.util.Arrays;
 
+/**
+ * Program to find the element with the value in middle
+ */
 public class MiddleElement {
     public static void main(String[] args) {
         int arr[] = {978, 518, 600, 100, 800};
-    int middleElement = middle(arr);
-        System.out.println("Middle Elemnet: "+middleElement);
+        int middleElement = middle(arr);
+        System.out.println("Middle Elemnet: " + middleElement);
     }
-    static int middle(int arr[]){
+
+    /**
+     * @param arr
+     * @return the middle element
+     */
+    static int middle(int arr[]) {
         Arrays.sort(arr);
-        int mid = 0+(arr.length-1)/2;
+        int mid = 0 + (arr.length - 1) / 2;
         return arr[mid];
     }
 }
--- src/array/MinMaxElement.java
+++ src/array/MinMaxElement.java
@@ -7,7 +7,7 @@ public class MinMaxElement {
     /**
      * Class used to return min and max element from getMinMax() method
      */
-    static class Pair{
+    static class Pair {
         private int min;
         private int max;
 
@@ -18,36 +18,34 @@ public class MinMaxElement {
     }
 
     /**
-     *
      * @param arr
      * @param start start index of array
-     * @param end last index of array
+     * @param end   last index of array
      * @return an object of {@code Pair} containing minimum and maximum element
      */
-    static Pair getMinMax(int arr[], int start, int end){
-        if(start == end)
+    static Pair getMinMax(int arr[], int start, int end) {
+        if (start == end)
             return new Pair(arr[start], arr[start]);
-        if(end == start + 1){
-            if(arr[start] > arr[end]){
+        if (end == start + 1) {
+            if (arr[start] > arr[end]) {
                 return new Pair(arr[end], arr[start]);
             } else {
                 return new Pair(arr[start], arr[end]);
             }
-        }
-        else{
-            int mid = (start + end)/2;
+        } else {
+            int mid = (start + end) / 2;
             Pair leftMinMax = getMinMax(arr, 0, mid);
-            Pair rightMinMax = getMinMax(arr, mid+1, end);
+            Pair rightMinMax = getMinMax(arr, mid + 1, end);
             int min = leftMinMax.min < rightMinMax.min ? leftMinMax.min : rightMinMax.min;
             int max = leftMinMax.max > rightMinMax.max ? leftMinMax.max : rightMinMax.max;
             return new Pair(min, max);
         }
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         int arr[] = {1000, 11, 445, 1, 330, 3000};
-        Pair result = getMinMax(arr, 0, arr.length-1);
-        System.out.println("Maximum element: "+result.max);
-        System.out.println("Minimum element: "+result.min);
+        Pair result = getMinMax(arr, 0, arr.length - 1);
+        System.out.println("Maximum element: " + result.max);
+        System.out.println("Minimum element: " + result.min);
     }
 }
