a4f1958cb753c5854ecc46551f3e923b03d7c856
prachigupta28 <74293469+prachigupta28@users.noreply.github.com>
2020-12-29 00:48:20 +0530

    refactor: formatted the code

--- src/array/KthSmallestElement.java
+++ src/array/KthSmallestElement.java
@@ -8,20 +8,20 @@ import java.util.Arrays;
 public class KthSmallestElement {
     /**
      * This function finds the Kth smallest element in an array
+     *
      * @param arr
-     * @param l start index of array
-     * @param r end index of array
+     * @param l   start index of array
+     * @param r   end index of array
      * @param k
      * @return kth smallest element in an array
      */
-    public static int kthSmallest(int[] arr, int l, int r, int k)
-    {
+    public static int kthSmallest(int[] arr, int l, int r, int k) {
         Arrays.sort(arr);
-        return arr[k-1];
+        return arr[k - 1];
     }
 
     public static void main(String[] args) {
         int arr[] = {5, -8, 10, 37, 101, 2, 9};
-        System.out.println("Kth Smallest Element: "+kthSmallest(arr, 0, 6, 6));
+        System.out.println("Kth Smallest Element: " + kthSmallest(arr, 0, 6, 6));
     }
 }
