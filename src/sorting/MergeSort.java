b64d0cb8a861f1c147c0db57359b42b32697c664
prachigupta28 <74293469+prachigupta28@users.noreply.github.com>
2021-02-12 00:08:14 +0530

    added logic to merge array using merge sort

---
+++ src/sorting/MergeSort.java
@@ -0,0 +1,56 @@
+package sorting;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MergeSort {
+    /**
+     * sort the given array using merge sort
+     *
+     * @param a
+     * @param n
+     */
+    public static void mergeSort(int[] a, int n) {
+        if (n < 2) {
+            return;
+        }
+        int mid = n / 2;
+        int[] l = new int[mid];
+        int[] r = new int[n - mid];
+        for (int i = 0; i < mid; i++) {
+            l[i] = a[i];
+        }
+        for (int i = mid; i < n; i++) {
+            r[i - mid] = a[i];
+        }
+        mergeSort(l, mid);
+        mergeSort(r, n - mid);
+        merge(a, l, r, mid, n = mid);
+    }
+
+    public static void merge(int[] a, int[] l, int[] r, int left, int right) {
+        int k = 0, i = 0, j = 0;
+        while ((i < left) && (j < right)) {
+            if (l[i] < r[j]) {
+                a[k++] = l[i++];
+            } else {
+                a[k++] = r[j++];
+            }
+        }
+        while (i < left) {
+            a[k++] = l[i++];
+        }
+        while (j < right) {
+            a[k++] = r[j++];
+        }
+    }
+
+    @Test
+    public void shouldMerge() {
+        int[] actual = {5, 1, 6, 2, 3, 4};
+        int[] expected = {1, 2, 3, 4, 5, 6};
+        MergeSort.mergeSort(actual, actual.length);
+        assertArrayEquals(expected, actual);
+    }
+}
